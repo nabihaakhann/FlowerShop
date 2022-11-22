cf6a6e975debc27749afb208a4c51e6f315362fb
nabihaakhann <99578490+nabihaakhann@users.noreply.github.com>
2022-11-22 20:03:21 +0530

    Create addToCart.js

---
+++ addToCart.js
@@ -0,0 +1,14 @@
+var addItemId = 0;
+function addToCart(item){
+    addItemId += 1;
+    var selectedItem = document.createElement('div');
+    selectedItem.classList.add('cartImg');
+    selectedItem.setAttribute('id',addItemId);
+    var img = document.createElement('img');
+    img.setAttribute('src',item.children[0].currentSrc);
+    var cartItems = document.getElementById('title');
+    selectedItem.append(img);
+    cartItems.append(selectedItem);
+}
+Footer
+© 2022 GitHub, Inc
